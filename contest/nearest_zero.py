d52019ca6fb3aa302872c899d3619440fa9e5d9f
legyan <levongeg@gmail.com>
2022-12-14 11:34:10 +0300

    after review 2

--- contest/nearest_zero.py
+++ contest/nearest_zero.py
@@ -1,13 +1,15 @@
-# ID 79393277
-def main():
+# ID 79423865
+def get_input():
     n = int(input())
     houses = [int(x) for x in input().split()]
+    return n, houses
 
+
+def nearest_zero(n, houses):
     first_zero = 0
     while houses[first_zero] != 0:
         houses[first_zero] = float('inf')
         first_zero += 1
-
     for i in range(first_zero, n):
         if not houses[i]:
             j = 1
@@ -24,8 +26,12 @@ def main():
                     k += 1
             except IndexError:
                 pass
+    return ' '.join((str(x) for x in houses))
 
-    print(' '.join((str(x) for x in houses)))
+
+def main():
+    print(nearest_zero(*get_input()))
+
 
 if __name__ == '__main__':
     main()
--- contest/sleight_of_hand.py
+++ contest/sleight_of_hand.py
@@ -1,5 +1,5 @@
-# ID 79393055
-def main():
+# ID 79424704
+def get_input():
     k = int(input())
     numbers = []
     for i in range(4):
@@ -8,14 +8,22 @@ def main():
                 numbers.append(int(x))
             except ValueError:
                 pass
+    return k, numbers
 
+
+def max_score(k, numbers):
     set_of_nums = set(numbers)
     max_score = 0
     sum_k = 2 * k
     for x in set_of_nums:
         if numbers.count(x) <= sum_k:
             max_score += 1
-    print(max_score)
+    return max_score
+
+
+def main():
+    print(max_score(*get_input()))
+
 
 if __name__ == '__main__':
     main()
