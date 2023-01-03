a8e380fe9fa99b0e1c0fdd76511a3a3ed7b718f6
legyan <levongeg@gmail.com>
2023-01-03 21:23:55 +0300

    adapter pattern added

---
+++ patterns/adapter/adapter.py
@@ -0,0 +1,27 @@
+class Square:
+    def __init__(self, side=0):
+        self.side = side
+
+def calculate_area(rc):
+    return rc.width * rc.height
+
+class SquareToRectangleAdapter:
+    def __init__(self, square):
+        self.square = square
+
+    @property
+    def width(self):
+        return self.square.side
+
+    @property
+    def height(self):
+        return self.square.side
+
+if __name__ == '__main__':
+    x, y = input().split()
+    sq = Square(int(x))
+    adapter = SquareToRectangleAdapter(sq)
+    res1 = calculate_area(adapter)
+    sq.side = int(y)
+    res2 = calculate_area(adapter)
+    print(f'{res1} {res2}')
