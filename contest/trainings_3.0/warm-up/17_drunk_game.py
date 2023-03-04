e34163e4443f9da9b208d2e9db0ccbafa91827e0
Levon Gegamyan <levongeg@gmail.com>
2023-03-04 22:05:36 +0300

    added task 17

---
+++ contest/trainings_3.0/warm-up/17_drunk_game.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+
+def drunk_game(first, second):
+    max_count = 10 ** 6
+    for i in range(1, max_count):
+        f, s = first.popleft(), second.popleft()
+        if f > s and not (f == 9 and s == 0) or (f == 0 and s == 9):
+            first.append(f)
+            first.append(s)
+        else:
+            second.append(f)
+            second.append(s)
+        if not first:
+            return 'second', i
+        elif not second:
+            return 'first', i
+    return 'botva'
+
+if __name__ == '__main__':
+    first_player = deque([int(x) for x in input().split()])
+    second_player = deque([int(x) for x in input().split()])
+    print(*drunk_game(first_player, second_player))
